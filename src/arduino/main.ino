b4e1bab1c586dfdf9849ea7b6c27b36a4e8386c7
AssoDePicche <samuelprado730@gmail.com>
2023-06-02 00:15:14 -0300

    refactor: update code

--- src/arduino/main.ino
+++ src/arduino/main.ino
@@ -10,8 +10,7 @@
 #define DHTTYPE          DHT22
 #define MAX_TEMPERATURE   38.5
 #define MIN_HUMIDITY        60
-#define BAUD_RATE         9600
-#define REFRESH_DELAY     2000
+#define REFRESH_DELAY      300
 #define LCD_COLUMNS         16
 #define LCD_LINES            2
 #define LCD_ADDRESS       0x27
@@ -20,23 +19,17 @@ Servo servo;
 
 DHT sensor(DHTPIN, DHTTYPE);
 
-int servo_position = 0;
-
-double humidity, temperature;
-
 LiquidCrystal_I2C display(LCD_ADDRESS, LCD_COLUMNS, LCD_LINES);
 
 void setup()
 {
-  pinMode(SWITCHPIN, INPUT);
-
   pinMode(HEATERPIN, OUTPUT);
+
+  digitalWrite(HEATERPIN, LOW);
 
   display.init();
 
   display.setBacklight(HIGH);
-
-  Serial.begin(BAUD_RATE);
 
   sensor.begin();
 
@@ -45,14 +38,20 @@ void setup()
 
 void loop()
 {
+  int heaterMode, servoPosition = 0;
+
+  double humidity, temperature;
+
+  display.setCursor(0, 0);
+
+  display.clear();
+
   humidity = sensor.readHumidity();
 
   temperature = sensor.readTemperature();
 
-  if (!is_the_sensor_working())
+  if (isnan(humidity) || isnan(temperature))
   {
-    Serial.println("Falha ao ler o sensor DHT");
-
     display.print("Falha ao ler o sensor DHT");
 
     return;
@@ -60,21 +59,10 @@ void loop()
 
   if (humidity < MIN_HUMIDITY)
   {
-    Serial.println("Baixa umidade");
-
     display.print("Baixa umidade");
+
+    return;
   }
-
-  display_data();
-
-  update_heater();
-
-  update_servo();
-}
-
-void display_data()
-{
-  display.setCursor(0, 0);
 
   display.print("Umidade: ");
 
@@ -84,34 +72,31 @@ void display_data()
 
   display.setCursor(0, 1);
 
-  display.print("Temperatura: ");
+  display.print("Temp: ");
 
   display.print(temperature);
 
-  display.print(" °C");
-}
+  display.print(" C");
 
-void update_heater()
-{
-  int mode = (temperature < MAX_TEMPERATURE) ? HIGH : LOW;
+  // the heating modes are reversed, so low means high and high means low
 
-  digitalWrite(HEATERPIN, mode);
-}
+  heaterMode = (temperature < MAX_TEMPERATURE) : LOW : HIGH;
 
-void update_servo()
-{
-  while (servo_position++ <= SERVO_MAX_DEGREE)
+  digitalWrite(HEATERPIN, heaterMode);
+
+  while (servoPosition++ <= SERVO_MAX_DEGREE)
   {
-    servo.write(servo_position);
+    servo.write(servoPosition);
+
+    delay(REFRESH_DELAY);
   }
 
-  while (servo_position-- != 0)
-  {
-    servo.write(servo_position);
-  }
-}
+  delay(REFRESH_DELAY);
 
-bool is_the_sensor_working()
-{
-  return !isnan(humidity) && !isnan(temperature);
+  while (servoPosition-- != 0)
+  {
+    servo.write(servoPosition);
+
+    delay(REFRESH_DELAY);
+  }
 }
